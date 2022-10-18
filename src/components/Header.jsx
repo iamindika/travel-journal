c0490f06463387e1716bdc3bb5970fa87cfa9828
Indika Tantrege <indika.kolamba@ca3cmam0f177wmd.ht.home>
2022-10-18 10:36:00 -0400

    remove container class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,13 @@
 import globeIcon from '../assets/images/globe-icon.png';
 
 const Header = () => (
-  <header className="header row">
-    <div className="container row row--center">
-      <img 
-        className="header-icon"
-        src={globeIcon}
-        alt="globe icon"
-      />
-      <h1 className="header-title">my travel journal.</h1>
-    </div>
+  <header className="header">
+    <img 
+      className="header-icon"
+      src={globeIcon}
+      alt="globe icon"
+    />
+    <h1 className="header-title">my travel journal.</h1>
   </header>
 );
 
